45016cb4c4f4e63cfec9179a1db27793da888bf9
Ben <bgrowcott@hotmail.co.uk>
2022-02-27 21:12:07 +0000

    set up promise in function to allow all inquirer question to complete - set up function to make array of employee objects

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const chalk = require("chalk");
-const employeeArray = [];
+const { resolve } = require("path");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const inquirerArray = [];
 
 const getUserName = () => {
   return inquirer.prompt([
@@ -142,63 +147,63 @@ const engineerInfo = () => {
 };
 
 const internInfo = () => {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "internName",
-        message: chalk.greenBright(`Please type in the intern's name:`),
-        validate(answer) {
-          if (!answer) {
-            return chalk.red(`At least type something in!`);
-          }
-          return true;
-        },
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: chalk.greenBright(`Please type in the intern's name:`),
+      validate(answer) {
+        if (!answer) {
+          return chalk.red(`At least type something in!`);
+        }
+        return true;
       },
-      {
-        type: "input",
-        name: "internId",
-        message: chalk.greenBright(`Please type in the intern's ID:`),
-        validate(answer) {
-          if (!answer) {
-            return chalk.red(`At least type something in!`);
-          }
-          return true;
-        },
+    },
+    {
+      type: "input",
+      name: "internId",
+      message: chalk.greenBright(`Please type in the intern's ID:`),
+      validate(answer) {
+        if (!answer) {
+          return chalk.red(`At least type something in!`);
+        }
+        return true;
       },
-      {
-        type: "input",
-        name: "internEmail",
-        message: chalk.greenBright(`Please type in the intern's email:`),
-        validate(answer) {
-          if (!answer) {
-            return chalk.red(`At least type something in!`);
-          }
-          return true;
-        },
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: chalk.greenBright(`Please type in the intern's email:`),
+      validate(answer) {
+        if (!answer) {
+          return chalk.red(`At least type something in!`);
+        }
+        return true;
       },
-      {
-        type: "input",
-        name: "internSchool",
-        message: chalk.greenBright(`Please type in the intern's School:`),
-        validate(answer) {
-          if (!answer) {
-            return chalk.red(`At least type something in!`);
-          }
-          return true;
-        },
+    },
+    {
+      type: "input",
+      name: "internSchool",
+      message: chalk.greenBright(`Please type in the intern's School:`),
+      validate(answer) {
+        if (!answer) {
+          return chalk.red(`At least type something in!`);
+        }
+        return true;
       },
-      {
-        type: "list",
-        name: "anotherEmployee",
-        choices: [
-          "Add an engineer",
-          "Add an intern",
-          "Finish building your team",
-        ],
-        message: chalk.greenBright(`Would you like to add another team member?`),
-      },
-    ]);
-  };
+    },
+    {
+      type: "list",
+      name: "anotherEmployee",
+      choices: [
+        "Add an engineer",
+        "Add an intern",
+        "Finish building your team",
+      ],
+      message: chalk.greenBright(`Would you like to add another team member?`),
+    },
+  ]);
+};
 
 // const generateHTML = ({}) =>
 //   `
@@ -209,22 +214,50 @@ const init = () => {
   getUserName()
     .then(managerInfo)
     .then(addAnother)
-    .then(returnArray)
+    .then(makeObjects)
     .catch((err) => console.error(err));
 };
 
 init();
 
 function addAnother(answers) {
-    employeeArray.push(answers)
-  if (answers.anotherEmployee === "Add an engineer") {
-    engineerInfo().then(addAnother);
-  }
-  if (answers.anotherEmployee === "Add an intern") {
-    internInfo().then(addAnother);
-  }
-  if (answers.anotherEmployee === "Finish building your team") {
-    console.log(employeeArray)
-  }
+  inquirerArray.push(answers);
+  return new Promise((resolve) => {
+    if (answers.anotherEmployee === "Add an engineer") {
+      resolve(engineerInfo().then(addAnother));
+    }
+    if (answers.anotherEmployee === "Add an intern") {
+      resolve(internInfo().then(addAnother));
+    }
+    if (answers.anotherEmployee === "Finish building your team") {
+      resolve(inquirerArray);
+    }
+  });
 }
 
+const employeeArray = [];
+function makeObjects(array) {
+  array.forEach((object) => {
+    if (Object.keys(object)[0] === "managerName") {
+      let { managerName, managerId, managerEmail, managerOfficeNumber } =
+        object;
+      employeeArray.push(
+        new Manager(managerName, managerId, managerEmail, managerOfficeNumber)
+      );
+    }
+    if (Object.keys(object)[0] === "engineerName") {
+      let { engineerName, engineerId, engineerEmail, engineerGithub } = object;
+      employeeArray.push(
+        new Engineer(engineerName, engineerId, engineerEmail, engineerGithub)
+      );
+    }
+    if (Object.keys(object)[0] === "internName") {
+      let { internName, internId, internEmail, internSchool } =
+        object;
+      employeeArray.push(
+        new Intern(internName, internId, internEmail, internSchool)
+      );
+    }
+  });
+  console.log(employeeArray);
+}
